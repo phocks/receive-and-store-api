94ab7dc9a1d0fbedd5e4ba370559abe3c91b16ea
Joshua Byrd <phocks@gmail.com>
2023-06-15 20:07:42 +1000

    Another filename

--- index.js
+++ index.js
@@ -2,21 +2,23 @@
 const express = require("express");
 const multer = require("multer");
 
-const upload = multer({ dest: "uploads/" });
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, 'uploads')
+  },
+  filename: function (req, file, cb) {
+    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
+    cb(null, file.fieldname + '-' + uniqueSuffix)
+  }
+})
+
+const upload = multer({ storage: storage })
+
+// const upload = multer({ dest: "uploads/" });
 
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
-const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, "/tmp/my-uploads");
-  },
-  filename: function (req, file, cb) {
-    const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
-    cb(null, file.fieldname + "-" + uniqueSuffix);
-  },
-});
 
 app.post("/upload", upload.single("file"), uploadFiles);
 
